b5c9ac2517168fe42c45e041c146eb9bf32b512a
RavenHex1296 <suanoo5196@gmail.com>
2024-01-13 21:40:53 +0000

    pulse monitor is a work in progress

--- Software/backend.js
+++ Software/backend.js
@@ -44,13 +44,68 @@ function scan_for_impact(new_data){
         console.log(`${sensor_string}.`)
     }
 
-    if (new_data[new_data.length - 1] < 50) {
-        console.log("Your heartbeat is dangerously low")
-    }
+    // if (new_data[new_data.length - 1] < 50) {
+    //     console.log("Your heartbeat is dangerously low")
+    // }
 
 
 }
 //setInterval(parse_data); 
 //setInterval(impact_detected)
 
+function count_pulse(pulse_data) {
+    for (i = 1; i < pulse_data.length; i++){
+        if (pulse_data[i] >= 580) {
+            const first_instance = i
+            break
+        }
+    }
+    let times_between_beats = []
+    let dropped = true     
+    let begin_count = false
+
+    for (i = 1; i < pulse_data.length; i++){
+        if (pulse_data[i] >= 580 && i != first_instance) {
+            
+        }
+    }
+
+
+}
+
+// function count_pulse(pulse_data){
+//     let times_between_beats = []
+//     let dropped = true
+//     let begin_count = false
+//     let first_passed = false
+
+//     for (i  = 0; i < pulse_data.length; i ++){
+//         if (pulse_data[i] >= 580 && dropped == true && begin_count == false) {
+//             if (first_passed == false) {
+//                 first_passed = true
+//                 let begin_time = Date.now() / 1000
+//                 begin_count = true
+//                 dropped = false
+//             }
+//             else{
+//                 times_between_beats.push(Date.now()/1000 - begin_time)
+//                 let begin_time = Date.now() / 1000
+//                 begin_count = true
+//                 dropped = false
+//             }
+//         }
+
+//         if (dropped == false && begin_count == true) {
+//             if (pulse_data[i] < 580) {
+//                 dropped = true
+//                 begin_count = false
+//             }
+//     }
+
+//     return times_between_beats
+// }
+// }
 scan_for_impact([25, 55, 255, 255, 20])
+
+
+console.log(count_pulse([720, 590, 600, 480, 581]))
