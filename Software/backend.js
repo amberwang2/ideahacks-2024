7ce5ee3f43c209d1bf5864d9e3a306afe4936c89
RavenHex1296 <suanoo5196@gmail.com>
2024-01-13 16:30:04 +0000

    Collection mechanism kind of created

--- Software/backend.js
+++ Software/backend.js
@@ -2,17 +2,18 @@ let all_data = {1: [], 2: [], 3: [], 4: []};
 
 //data comes in at sensor 1, sensor 2, sensor 3, sensor 4, end, repeat
 let grouped_data = []
+
 function collect_one_interation_data(data) {
     grouped_data.push(data)
 
     if (grouped_data.length == 4) {
         update_data(grouped_data)
+        scan_data(grouped_data)
         grouped_data = []
     }
 }
 
 function update_data(new_data) {  
-
     for (let i = 0; i < new_data.length; i++) {
         all_data[i + 1].push(new_data[i])
       }
@@ -43,8 +44,5 @@ function scan_for_impact(new_data){
         console.log(`${sensor_string}.`)
     }
 }
-
-sample_data = [255, 0, 100, 255]
-scan_for_impact(sample_data)
 //setInterval(parse_data); 
 //setInterval(impact_detected)
